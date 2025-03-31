b4292989379da04dccfa78520def04ffa7379b4e
Stephen Nnamani <155476794+stephenscripts@users.noreply.github.com>
2025-03-31 12:18:45 +0200

    Updated

---
+++ JavaScripts/weatherApi.js
@@ -0,0 +1,12 @@
+var reciever = fetch("https://api.data.gov.sg/v1/environment/air-temperature").then(Response => {
+    if(!Response.ok){throw new Error("'Network response was not ok'")
+    } return Response.json().then(response => response.json())
+    .then(data => {
+      const resultDiv = document.getElementById('result');
+      resultDiv.innerHTML = `<p><strong>Title:</strong> ${data.title}</p>
+                             <p><strong>Body:</strong> ${data.body}</p>`;
+    })
+    .catch(error => console.error('Error:', error));
+})
+
+console.log(reciever)
